840289c4d5fc776cc8f33e4125358f9a276265f4
sharon <sharon329@naver.com>
2025-08-17 19:16:05 +0900

    feat(thread): Demonstrate direct call to run() executes on main thread

---
+++ src/main/java/com/example/javathreadnotes/s1_thread/ThreadEx3.java
@@ -0,0 +1,25 @@
+package com.example.javathreadnotes.s1_thread;
+
+/*
+* 새로운 스레드가 생성되지 않고 main 스레드에서 일반 메서드처럼 실행
+* */
+public class ThreadEx3 {
+    public static void main(String[] args) throws Exception{
+        ThreadEx3_1 t1 = new ThreadEx3_1();
+        t1.run(); // run() 메서드를 직접 호출
+    }
+}
+
+class ThreadEx3_1 extends Thread {
+    public void run() {
+        throwException();
+    }
+
+    public void throwException() {
+        try {
+            throw new Exception();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
